7ba01bfe2464c7ef43cb8977521c8586de4f9afe
Touutae-lab <pantakan.totae@gmail.com>
2022-10-17 22:36:18 +0700

    black

--- fastgrad_api/student/suggest.py
+++ fastgrad_api/student/suggest.py
@@ -40,7 +40,9 @@ async def postSuggest() -> dict:
         learned_course.append(utility.getCategory(i)[0])
 
     possible_course = utility.findPossibleCourse(learned_course, all_course)
-    term_2 = utility.suggestion(possible_course, requirement, learned_course, term_id=2)
+    term_2 = utility.suggestion(
+        possible_course, requirement, learned_course, term_id=2
+    )
 
     return {"term_1": term_1, "term_2": term_2}
 
@@ -57,7 +59,9 @@ async def testStudent() -> dict:
         learned_course.append(utility.getCategory(i)[0])
 
     possible_course = utility.findPossibleCourse(learned_course, all_course)
-    term_2 = utility.suggestion(possible_course, requirement, learned_course, term_id=2)
+    term_2 = utility.suggestion(
+        possible_course, requirement, learned_course, term_id=2
+    )
     return {"term_1": term_1, "term_2": term_2}
 
 
