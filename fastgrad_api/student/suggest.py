c7806425a941d89c1fdcef0e4e322dd12296de54
Touutae-lab <pantakan.totae@gmail.com>
2022-10-17 16:47:13 +0700

    checked file

--- fastgrad_api/student/suggest.py
+++ fastgrad_api/student/suggest.py
@@ -1,3 +1,7 @@
+import random
+
+from pkg_resources import require
+
 from database import db
 from flask import Blueprint, request, request_finished
 from supertokens_python.recipe.session.framework.flask import verify_session
@@ -6,12 +10,14 @@ from . import utility
 
 blueprint: Blueprint = Blueprint("/suggest", __name__)
 
+rank: list = [5, 4, 3]
+
+
 """"
 Utility Functions
 #################
 
 """
-
 
 # Random Course that have equivilent weight
 def randomFromAction(id):
@@ -41,22 +47,59 @@ Returns:
 
 
 @blueprint.route("/suggest", methods=["POST"])
-@verify_session()
-def postSuggest() -> dict:
+async def postSuggest() -> dict:
     req = request.args.to_dict()
-    all_course = utility.getCourse(req["plan"])
+    
+    all_course = utility.getCourse(req["plan_id"])
     learned_course = utility.getUserEnrollment(req["student_id"])
     possible_course = utility.findPossibleCourse(learned_course, all_course)
+    requirement = utility.getPlanRequirment(req["plan_id"])
+
+    term_1 = utility.suggestion(possible_course, requirement, learned_course)
+    
+    for i in term_1:
+        learned_course.append(utility.getCategory(i)[0])
+    
+    possible_course = utility.findPossibleCourse(learned_course, all_course)
+    term_2 = utility.suggestion(possible_course, requirement, learned_course)
+    
     return {
-        "total_course": len(learned_course),
-        "learned_course": learned_course,
+        "term_1": term_1,
+        "term_2": term_2
     }
 
 
 @blueprint.route("/trytotest", methods=["GET"])
-def testStudent() -> dict:
+async def testStudent() -> dict:
+    learned_course = utility.getUserEnrollment("630510501")
     all_course = utility.getCourse()
-    # learned_course = utility.getUserEnrollment()
-    # possible_course = utility.findPossibleCourse(learned_course, all_course)result = utility.getPlanRequirment()
-    # result = utility.findPossibleCourse(learned_course, all_course)
-    return {"course": all_course}
+    possible_course = utility.findPossibleCourse(learned_course, all_course)
+    
+    requirement = utility.getPlanRequirment()
+    # test = utility.diffRequirment(requirement, learned_course)
+    
+    term_1 = utility.suggestion(possible_course, requirement, learned_course)
+    
+    for i in term_1:
+        learned_course.append(utility.getCategory(i)[0])
+    
+    possible_course = utility.findPossibleCourse(learned_course, all_course)
+    term_2 = utility.suggestion(possible_course, requirement, learned_course)
+    return {"term_1": term_1,
+            "term_2": term_2
+            }
+
+
+@blueprint.route("/available_course", methods=["POST"])
+async def avalable_course() -> dict:
+    req = request.args.to_dict()
+    learned_course = utility.getUserEnrollment(req["student_id"])
+    all_course = utility.getCourse(req["plan_id"])
+    possible_course = utility.findPossibleCourse(learned_course, all_course)
+    return {"course": possible_course}
+
+
+@blueprint.route("/should_learn", methods=["GET"])
+def should_learn() -> dict:
+    
+    return
--- fastgrad_api/student/utility.py
+++ fastgrad_api/student/utility.py
@@ -1,14 +1,16 @@
-from database import db
 
+from database import db
+import random
 
 def getUserEnrollment(studentId: str) -> list:
     query: str = f"""SELECT course_id, category_id FROM enrollment
-    WHERE student_id = %d"""
+    WHERE student_id = %s AND grade != 'F'"""
     cursor = db.cursor()
-    cursor.execute(query)
+    cursor.execute(query, [studentId])
 
     result = cursor.fetchall()
     db.commit()
+
     return result
 
 
@@ -26,29 +28,101 @@ def getCourse(course: int = 1) -> list:
     return result
 
 
-def findPrerequisite(course: dict):
-    return
-
-
-def findPostrequisite():
-    return
+def findPrerequisite(course):
+    query: str = "SELECT precourse_id, pregroup_id FROM `prerequisite` WHERE course_id = %s"
+    cursor = db.cursor()
+    cursor.execute(query, [course])
+    
+    data = cursor.fetchall()
+    db.commit()
+    
+    result  = {}
+    
+    if len(data) == 1:
+        return data
+    
+    for i in data:
+        if i[1] not in result:
+            result.update({i[1] : [i[0]]})
+        elif i[1] in result:
+            result[i[1]].append(i[0])
+    return result
 
 
 def findPossibleCourse(learnedCourse: list, allCourse: list):
     can_learn = []
-    not_decide = []
-    for value in allCourse:
-        if value not in learnedCourse:
-            not_decide.append(value)
-    for value in not_decide:
-        if not_decide:
-            can_learn.append(value)
+    learnedCourse = normallize(learnedCourse)
+    result = []
+    for i in allCourse:
+        if i not in learnedCourse:
+            can_learn.append(i)
+    for i in can_learn:
+        if canLearn(i, learnedCourse):
+            result.append(i)
+    result  = set(result)
+    pseudo = removeCourse()
+    for i in pseudo:
+        result.discard(i)
+    return list(result)
 
-    return can_learn
 
+def suggestion(possiblecourse: list, requirement: dict, learned_course, term_id = 1):
+    norm_possible = normallize_requirement(possiblecourse)
+    norm_cred = normCred(learned_course)
+    
+    for i in requirement:
+        if i in norm_cred:
+            requirement.update({i: requirement[i] - norm_cred[i]})
+    
+    need_learn = [i for i in requirement if requirement[i] != 0]
+    
+    
+    result = []
+    for i in norm_possible[5]:
+        result.append(i)
+    
+    suggest = random.sample(result, 3)
+    
+    # c = random.sample(need_learn, 4)
+    
+    # for i in c:
+    #     if i not in norm_possible:
+    #         pass
+    #     else:
+    #         token = random.sample(norm_possible[int(i)], 1)
+    #         if token[0] not in suggest:
+    #             suggest.append(token[0])
+    while len(suggest) < 8:
+        c = random.sample(need_learn, 1)
+        if int(c[0]) in norm_possible:
+            token = random.sample(norm_possible[int(c[0])], 1)
+            if token[0] not in suggest:
+                suggest.append(token[0])
+                
+    return suggest
+    # if term_id == 1:
+    #     for i in result:
+    #         if checkOpen(i)[0][0] == 1:
+    #             pass
+    #         else:
+    #             result.remove(i)
+    #     if len(result) > 7:
+    #         return random.sample(result, 7)
+    #     else:
+    #         return random.sample(need_learn, 7 - len(result))
 
-def suggestion(Possiblecourse: dict, requirement: dict):
-    return
+    # if term_id == 2:
+    #     for i in result:
+    #         if checkOpen(i)[0][1] == 1:
+    #             pass
+    #         else:
+    #             result.remove(i)
+    #     if len(result) > 7:
+    #         return random.sample(result, 7)
+    #     else:
+    #         return random.sample(need_learn, 7 - len(result))
+    # return result
+    
 
 
 def getPlanRequirment(course: int = 1) -> dict:
@@ -64,6 +138,121 @@ def getPlanRequirment(course: int = 1) -> dict:
 
     result = {}
     for couple in data:
-        result.update({couple[0]: [couple[1]]})
+        result.update({couple[0]: couple[1]})
 
     return result
+
+
+# Due the DB confusion this is the result for suggestion code LUL
+def canLearn(course: str, learned_course):
+    pre = findPrerequisite(course)
+    
+    
+    query: str = "SELECT inclusion_type FROM prerequisite_group WHERE group_no = %s"
+    cursor = db.cursor()
+
+
+    if len(pre) == 0:
+        return True
+    
+    if len(pre) == 1:
+        if pre[0][0] in learned_course:
+            return True
+        else:
+            return False
+        
+    for i in pre:
+        cursor.execute(query, [i])
+        data = cursor.fetchall()
+        db.commit()
+        
+        if data[0][0] == "any":
+            for j in pre[i]:
+                if j in learned_course:
+                    return True
+        if data[0][0] == "all":
+            count = 0
+            for j in pre[i]:
+                    if j in learned_course:
+                        count += 1
+            if len(pre[i]) == count:
+                return True
+    return False
+
+
+def normallize(course):
+    return [data[0] for data in course]
+
+
+def removeCourse():
+    query: str = "SELECT * FROM pseudo_course"
+    cursor = db.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    db.commit()
+    result = normallize(data)
+    return result
+
+
+def normallize_requirement(possible_course):
+    query: str = "SELECT * FROM default_course_category WHERE course_id = %s"
+    cursor = db.cursor()
+    
+    result = {}
+    for i in possible_course:
+        cursor.execute(query, [i])
+        data = cursor.fetchall()
+        db.commit()
+        
+        if data[0][1] not in result:
+            result.update({data[0][1] : [data[0][0]]})
+        elif data[0][1] in result:
+            result[data[0][1]].append(data[0][0])
+    return result
+
+
+    """_summary_
+    "course_category_id" : ""
+    """
+def getCreditCourse(course):
+    query: str = "SELECT id, credit FROM course where id = %s"
+    cursor = db.cursor()
+    
+    cursor.execute(query, [course])
+    data = cursor.fetchall()
+    db.commit()
+
+    return data[0][1]
+
+
+def normCred(learned_course):
+    result = {}
+    for i in learned_course:
+        key = i[1]
+        value = getCreditCourse(i[0])
+        if key not in result:
+            result.update({key: value})
+        result.update({key: result[key] + value})
+        
+    return result
+
+
+def checkOpen(course):
+    query: str = "SELECT term_1, term_2 FROM course where id = %s"
+    cursor = db.cursor()
+    
+    cursor.execute(query, [course])
+    data = cursor.fetchall()
+    db.commit()
+
+    return data
+
+
+def getCategory(course):
+    query: str = "SELECT * FROM `default_course_category` WHERE course_id = %s"
+    cursor = db.cursor()
+    
+    cursor.execute(query, [course])
+    data = cursor.fetchall()
+    db.commit()
+    return data
