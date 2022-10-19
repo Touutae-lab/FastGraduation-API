2069f0b22d9ac5262199514f1134c59926f8947a
Saksinkarn Petchkuljinda <dward.the.2nd@gmail.com>
2022-10-19 23:19:56 +0700

    Add checking student academic year

--- fastgrad_api/student/browse_course.py
+++ fastgrad_api/student/browse_course.py
@@ -1,5 +1,6 @@
 from database import db
-from flask import Blueprint, request
+from flask import Blueprint, g, request
+from supertokens_python.recipe.session import SessionContainer
 from supertokens_python.recipe.session.framework.flask import verify_session
 
 blueprint: Blueprint = Blueprint("browse_course", __name__)
@@ -8,12 +9,24 @@ blueprint: Blueprint = Blueprint("browse_course", __name__)
 @blueprint.route("/browse_course", methods=["GET"])
 @verify_session()
 def browse_course() -> dict:
+    session: SessionContainer = g.supertokens
+    user_id = session.get_user_id()
+    cursor = db.cursor()
+
+    query = "SELECT academic_year FROM student WHERE user_id = %s"
+    cursor.execute(query, [user_id])
+    stu_year = cursor.fetchall()[0]
+
+    plan_id = 3
+    if stu_year == 2020:
+        plan_id = 1
+
     query = (
         "SELECT id, name_th, name_en, category_id "
         "FROM course "
         "JOIN default_course_plan ON id = default_course_plan.course_id "
         "LEFT JOIN default_course_category ON id = default_course_category.course_id "
-        "WHERE plan_id = 1 AND "
+        "WHERE plan_id = %(plan_id)s AND "
         '      (id LIKE "%%%(kw)s%%" OR '
         '       name_en LIKE "%%%(kw)s%%" OR '
         '       name_th LIKE "%%%(kw)s%%")'
@@ -23,13 +36,11 @@ def browse_course() -> dict:
     if "q" in params:
         kw = params["q"]
 
-    cursor = db.cursor()
-    cursor.execute(query % {"kw": kw})
+    cursor.execute(query % {"kw": kw, "plan_id": plan_id})
 
     res_courses = cursor.fetchall()
 
     query = "SELECT id, abbr_th, abbr_en FROM course_category"
-    cursor = db.cursor()
     cursor.execute(query)
     res_cat = cursor.fetchall()
 
